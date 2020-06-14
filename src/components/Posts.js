ee62de7ad9184370e38dc12ed011b5e230ac044a
Alexander Bercow <alexanderbercow@AlexandMBPBlack.home>
2020-06-14 15:40:42 -0400

    added structure to Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,20 +8,32 @@ import {
 
   
 const Posts = props => (
-    <div>
-      <h1>Name: Jadon Sancho</h1>
-      <h3>Version: TOTS</h3>
-      <h4>Club: BVB</h4>
-      <h4>Nation: England</h4>
-      <h4>Skills: 5* // Weak Foot: 4*</h4>
-      <h4>Foot: Right</h4>
-      <h4>Def. WR: Low // Att. WR: Med</h4>
-      <h4>Traits: Technical Dribbler (CPU AI Only) // Outside Foot Shot // Flair // Finesse Shot</h4>
-      <p>[In Game Stats Go Here]</p>
-      <p>Review written by me goes here</p>
-      <p>Pace: 9.7/10 // Shooting: 9.3/10 // Passing: 9/10 // Dribbling 9.4/10 // Defense: N/A // Physical: 7.4/10
-      </p>
-
+    <div className="wrapper">
+      <div className="image-column"></div>
+      <div className="info-column">
+        <div className="player-main">
+          <h1>Name: Jadon Sancho</h1>
+          <p>Version: TOTS</p>
+          <p>Club: BVB</p>
+          <p>Nation: England</p>
+          <p>Age: 20</p>
+        </div>
+        <div className="player-meta">
+          <p>Skills: 5* // Weak Foot: 4*</p>
+          <p>Foot: Right</p>
+          <p>Def. WR: Low // Att. WR: Med</p>
+          <p>Traits: Technical Dribbler (CPU AI Only) // Outside Foot Shot // Flair // Finesse Shot</p>
+        </div>
+        <div className="player-stats">
+          <p>[[In Game Stats Go Here]]</p>  
+        </div>
+        <div className="player-review">
+          <h4>Review written by me goes here</h4>
+          <p>Comparisons: </p>
+          <h2>Pace: 9.7/10 // Shooting: 9.3/10 // Passing: 9/10 // Dribbling 9.4/10 // Defense: N/A // Physical: 7.4/10
+          </h2>
+        </div>
+      </div>
     </div>
 )
 

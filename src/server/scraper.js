5d6c7f5041025d4420717c1fa12ad3570b2aa391
Alexander Bercow <alexander@bercow.com>
2020-07-28 00:49:38 -0400

    spent hours trying to get the scraper to work with jsdom and getElementbyID or querySelector

--- src/server/scraper.js
+++ src/server/scraper.js
@@ -1,5 +1,32 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const got = require('got');
+const request = require('request')
+const { JSDOM } = require( "jsdom" );
+// const { window } = new JSDOM( "" );
+// const $ = require( "jquery" )( window );
+
+
+const req_url= 'https://www.futbin.com/20/player/48746/arthur-masuaku';
+const fetchImg = async () => {
+request({uri: req_url}, function(error, response, body){
+	if(!error && response.statusCode === 200){
+    let {window} = new JSDOM(response.body);
+    window.onload = () => {
+    let img = window.document.querySelector('#Player-card')
+    console.log(img)
+    console.log(window.getComputedStyle(img, null))
+    }
+    // let text = dom.window.getComputedStyle(imgFinder, null);
+    // console.log(imgFinder)
+    // const img = dom.window.getComputedStyle(imgFinder, null).getPropertyValue('display');
+		// console.log(img);
+	}
+});
+}
+fetchImg();
+
+
 
 
 const fetchData = async (url) => {
@@ -13,7 +40,7 @@ const getResults = async (url) => {
   const scrapedPlayerInfo = {}
   
   const playerMetadata = $('tr')
-  playerMetadata.each((i, element) => {
+  playerMetadata.each((i, element) => {w
     const metadataName = $(element).find('th').text().trim()
     const metadataValue = $(element).find('td').text().trim()
     scrapedPlayerInfo[metadataName] = metadataValue;

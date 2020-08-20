12a9e2a9dbb6cef7404046c6bab9bb84890eb8ad
Alexander Bercow <alexander@bercow.com>
2020-08-20 18:28:40 -0400

    added -no sandbox to puppeteer launch, added puppeterr build pack

--- src/server/scraper.js
+++ src/server/scraper.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 
 
 const fetchImg = async (url) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox']);
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'domcontentloaded' });
   const data = await page.evaluate(() => {
